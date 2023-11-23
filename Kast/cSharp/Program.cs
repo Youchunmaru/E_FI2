ab2cbb8cfb603e654a805008f7970157e711d0cb
Samuel Gröner <38959861+Youchunmaru@users.noreply.github.com>
2023-11-23 11:39:47 +0000

    refac old solutions

--- Kast/cSharp/Program.cs
+++ Kast/cSharp/Program.cs
@@ -3,7 +3,13 @@
     class Program
     {
         public static void Main(String[] args){
-            DataTypes.Task4ABJKL.Run();
+            //DataTypes.Task4ABJKL.Run();
+            //Branching.SwitchCase.Run();
+            //Loops.Task6AB.Run();
+            //Loops.Task6CDEF.Run();
+            Loops.Task6G.Run();
+            //Loops.Task6H.Run();
+            Loops.Task6I.Run();
         }
     }
 }
