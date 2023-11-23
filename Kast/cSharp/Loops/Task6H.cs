bbe56641ba896035c44b466269c80b5c05ae33d6
Samuel Gröner <38959861+Youchunmaru@users.noreply.github.com>
2023-11-23 11:38:21 +0000

    task solution for 6h

---
+++ Kast/cSharp/Loops/Task6H.cs
@@ -0,0 +1,25 @@
+using Util;
+namespace Loops{
+    class Task6H{
+        public static void Run(){
+            Task();
+        }
+
+        public static void Task(){
+            Random random = new Random();
+            int count = 0;
+            for(int i = 0; i < 100; i++){
+                int noshows = 0;
+                for(int k = 0; k < 78; k++){
+                    if(random.NextDouble() > 0.95){
+                        noshows++;
+                    }
+                }
+                if(noshows > 3){
+                    count++;
+                }
+            }
+            Console.WriteLine($"In 100 shows his sheme didn't work {count} times");
+        }
+    }
+}
